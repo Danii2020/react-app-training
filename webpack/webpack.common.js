8a18a68e75ed3fa8aec5126d6e54a096286a187d
Danii2020 <danielmauricioerazoespinoza@gmail.com>
2022-07-13 12:34:08 -0500

    webpack files chunked

---
+++ webpack/webpack.common.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, '..', './src/index.tsx'),
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        path: path.resolve(__dirname, '..', './dist'),
+        filename: 'bundle.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '..', './src/index.html'),
+        }),
+    ],
+
+}
