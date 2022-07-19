c01b26c6aae2b11ab8ada5f659e0988e3038b94b
Danii2020 <danielmauricioerazoespinoza@gmail.com>
2022-07-19 10:13:17 -0500

    eslintrc and nvmrc added

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,27 +1,28 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: path.resolve(__dirname, '..', './src/index.tsx'),
-    module: {
-        rules: [
-            {
-                test: /\.(ts|js)x?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        path: path.resolve(__dirname, '..', './dist'),
-        filename: 'bundle.js',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '..', './public/index.html'),
-        }),
+  entry: path.resolve(__dirname, '..', './src/index.tsx'),
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
     ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, '..', './dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '..', './public/index.html'),
+    }),
+  ],
 };
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,9 +1,11 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-dynamic-require */
 const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.common');
 
 module.exports = (enVars) => {
-    const { env } = enVars;
-    const envConfig = require(`./webpack.${env}.js`);
-    const config = merge(commonConfig, envConfig);
-    return config;
+  const { env } = enVars;
+  const envConfig = require(`./webpack.${env}.js`);
+  const config = merge(commonConfig, envConfig);
+  return config;
 };
